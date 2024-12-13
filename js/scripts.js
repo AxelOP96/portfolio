5bd574b5d0109a6287a8e34fe8ef46846de9a4ad
Axel <axeelleguero@gmail.com>
2024-12-13 18:41:58 -0300

    Animations, page 6 finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-const popup = document.getElementById('popup');
+/* const popup = document.getElementById('popup');
 let counter = 0;
 const messages = [
   {
@@ -48,6 +48,76 @@ popup.addEventListener('transitionend', e => {
       generatePopUp(messages[counter]);
     }, 3000);
   }
-});
+}); */
 
 //generatePopUp(messages[counter]);
+//////////////////////////////
+const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
+
+pageTurnBtn.forEach((elem, index) => {
+  elem.onclick = () => {
+    const pageTurnId = elem.getAttribute('data-page');
+    const pageTurn = document.getElementById(pageTurnId);
+
+    if(pageTurn.classList.contains('turn')){
+      pageTurn.classList.remove('turn');
+      setTimeout(() => {
+        pageTurn.style.zIndex = 20 - index;
+      }, 500);
+    }
+    else{
+      pageTurn.classList.add('turn');
+      setTimeout(() => {
+        pageTurn.style.zIndex = 20 + index;
+      }, 500);
+    }
+  }
+})
+//////////////////////////////
+const pages = document.querySelectorAll('.book-page.page-right');
+const contactMeBtn = document.querySelector('.btn.contact-me');
+
+contactMeBtn.onclick = () => {
+  pages.forEach((page, index) => {
+    setTimeout(() => {
+      page.classList.add('turn');
+
+      setTimeout(() => {
+        page.style.zIndex = 20 + index;
+      }, 500)
+    }, (index + 1) *200 +100)
+  })
+}
+//////////////////////////////
+let totalPages = pages.length;
+let pageNumber = 0;
+
+function reverseIndex(){
+  pageNumber--;
+  if(pageNumber < 0){
+    pageNumber= totalPages -1;
+  }
+}
+//////////////////////////////
+const backProfileBtn = document.querySelector('.back-profile');
+
+backProfileBtn.onclick = () => {
+  pages.forEach((_, index) => {
+    setTimeout(() => {
+      reverseIndex();
+      pages[pageNumber].classList.remove('turn');
+
+      setTimeout(() => {
+        pages[pageNumber].style.zIndex = 10 + index;
+      }, 500)
+
+    }, (index + 1) * 200 + 100)
+  })
+}
+
+//////////////////////////////////////
+const coverRight = document.querySelector('.cover.cover-right');
+
+setTimeout(() => {
+  coverRight.classList.add('turn');
+}, 2100)
